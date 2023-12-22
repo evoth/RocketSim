31ed2a9609427e0b5b9f81ba20b82379ffbef8bc
Ethan Voth <ethanvoth1@gmail.com>
2023-12-22 03:42:59 -0700

    Remove intermediary classes

--- src/JSWrapper.cpp
+++ src/JSWrapper.cpp
@@ -1,116 +1,10 @@
-#include "JSWrapper.h"
-
-// TODO: Add comments
-
-Game::Game(std::string collisionMeshesFolder, CarConfigPreset preset)
-{
-    RocketSim::Init(collisionMeshesFolder.c_str());
-
-    arena = Arena::Create(GameMode::SOCCAR);
-
-    carConfigPreset = preset;
-    car = arena->AddCar(Team::BLUE, GetCarConfig());
-}
-
-Game::~Game()
-{
-    delete arena;
-}
-
-CarControls Game::GetControls()
-{
-    return car->controls;
-}
-
-void Game::SetControls(CarControls controls)
-{
-    car->controls = controls;
-}
-
-void Game::Step(int ticksToSimulate)
-{
-    CarState carState = car->GetState();
-    carState.boost = 100;
-    car->SetState(carState);
-    arena->Step(ticksToSimulate);
-}
-
-GameState Game::GetState()
-{
-    GameState state = {};
-
-    BallState ballState = arena->ball->GetState();
-    state.ballPos = ballState.pos;
-    state.ballAng = Angle::FromRotMat(ballState.rotMat);
-    state.ballVel = ballState.vel;
-    state.ballAngVel = ballState.angVel;
-
-    CarState carState = car->GetState();
-    state.carPos = carState.pos;
-    state.carAng = Angle::FromRotMat(carState.rotMat);
-    state.carVel = carState.vel;
-    state.carAngVel = carState.angVel;
-
-    for (int i = 0; i < 4; i++)
-    {
-        btTransform wheelTransform = carState.wheelTransforms[i];
-        state.wheelPos[i] = wheelTransform.getOrigin() * BT_TO_UU;
-        state.wheelAng[i] = Angle::FromRotMat(wheelTransform.getBasis());
-    }
-
-    return state;
-}
-
-void Game::ResetToKickoff()
-{
-    arena->ResetToRandomKickoff();
-}
-
-CarConfig Game::GetCarConfig()
-{
-    switch (carConfigPreset)
-    {
-    case CarConfigPreset::OCTANE:
-        return CAR_CONFIG_OCTANE;
-    case CarConfigPreset::DOMINUS:
-        return CAR_CONFIG_DOMINUS;
-    case CarConfigPreset::PLANK:
-        return CAR_CONFIG_PLANK;
-    case CarConfigPreset::BREAKOUT:
-        return CAR_CONFIG_BREAKOUT;
-    case CarConfigPreset::HYBRID:
-        return CAR_CONFIG_HYBRID;
-    case CarConfigPreset::MERC:
-        return CAR_CONFIG_MERC;
-    }
-}
+#include "RocketSim.h"
 
 #include "bind.h"
 using namespace emscripten;
 
 EMSCRIPTEN_BINDINGS(rocketsim)
 {
-    class_<Game>("Game")
-        .constructor<std::string, CarConfigPreset>()
-        .function("GetControls", &Game::GetControls)
-        .function("SetControls", &Game::SetControls)
-        .function("Step", &Game::Step)
-        .function("GetState", &Game::GetState)
-        .function("ResetToKickoff", &Game::ResetToKickoff)
-        .function("GetCarConfig", &Game::GetCarConfig);
-
-    value_object<GameState>("GameState")
-        .field("ballPos", &GameState::ballPos)
-        .field("ballAng", &GameState::ballAng)
-        .field("ballVel", &GameState::ballVel)
-        .field("ballAngVel", &GameState::ballAngVel)
-        .field("carPos", &GameState::carPos)
-        .field("carAng", &GameState::carAng)
-        .field("carVel", &GameState::carVel)
-        .field("carAngVel", &GameState::carAngVel)
-        .field("wheelPos", &GameState::wheelPos)
-        .field("wheelAng", &GameState::wheelAng);
-
     value_object<CarControls>("CarControls")
         .field("throttle", &CarControls::throttle)
         .field("steer", &CarControls::steer)
@@ -159,14 +53,6 @@ EMSCRIPTEN_BINDINGS(rocketsim)
         .field("wheelRadius", &WheelPairConfig::wheelRadius)
         .field("suspensionRestLength", &WheelPairConfig::suspensionRestLength)
         .field("connectionPointOffset", &WheelPairConfig::connectionPointOffset);
-
-    enum_<CarConfigPreset>("CarConfigPreset")
-        .value("OCTANE", CarConfigPreset::OCTANE)
-        .value("DOMINUS", CarConfigPreset::DOMINUS)
-        .value("PLANK", CarConfigPreset::PLANK)
-        .value("BREAKOUT", CarConfigPreset::BREAKOUT)
-        .value("HYBRID", CarConfigPreset::HYBRID)
-        .value("MERC", CarConfigPreset::MERC);
 
     constant("BALL_COLLISION_RADIUS_SOCCAR", RLConst::BALL_COLLISION_RADIUS_SOCCAR);
 }
